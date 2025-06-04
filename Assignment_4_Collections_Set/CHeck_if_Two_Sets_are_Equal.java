14829af3c36f29c20ef995334652a2ef265c9499
rohitjobs00 <rohitjobs00@gmail.com>
2025-06-04 20:03:06 +0530

    commit
    add

--- Assignment_4_Collections_Set/CHeck_if_Two_Sets_are_Equal.java
+++ Assignment_4_Collections_Set/CHeck_if_Two_Sets_are_Equal.java
@@ -3,17 +3,17 @@ import java.util.HashSet;
 
 public class CHeck_if_Two_Sets_are_Equal {
     public static void main(String[] args) {
-        HashSet<Integer> hs1 = new HashSet<>();
-        hs1.addAll(Arrays.asList(1,2,3));
-        System.out.println("Set 1: " + hs1);
+        HashSet<Integer> SetA = new HashSet<>();
+        SetA.addAll(Arrays.asList(1,2,3));
+        System.out.println("Set A: " + SetA);
 
-        HashSet<Integer> hs2 = new HashSet<>();
-//        hs2.addAll(Arrays.asList(3,2,1));
-        hs2.add(3);
-        hs2.add(2);
-        hs2.add(1);
-        System.out.println("Set 2: " + hs2);
+        HashSet<Integer> SetB = new HashSet<>();
+//        SetB.addAll(Arrays.asList(3,2,1));
+        SetB.add(3);
+        SetB.add(2);
+        SetB.add(1);
+        System.out.println("Set B: " + SetB);
 
-        System.out.println("Are the two sets equal: " + hs1.equals(hs2));
+        System.out.println("Are the two sets equal: " + SetA.equals(SetB));
     }
 }
